e38b5afbec056f62de09ba7077539e7d4294cb70
barel31 <barel123133@gmail.com>
2025-03-05 21:15:25 +0200

    try yt-dlp_linux

--- api/webhook.js
+++ api/webhook.js
@@ -17,7 +17,7 @@ let ytDlpPath, ffmpegPath;
 
 if (isVercel) {
   // Use Linux binaries when running on Vercel
-  ytDlpPath = path.join(__dirname, '..', 'bin', 'yt-dlp_linux_armv7l');
+  ytDlpPath = path.join(__dirname, '..', 'bin', 'yt-dlp_linux');
   ffmpegPath = path.join(__dirname, '..', 'bin', 'ffmpeg-linux');
 } else {
   // Use Windows binaries when developing locally
