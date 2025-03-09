5d93c9aac90929b84105aa029a3fa7a0adcac7a1
barel31 <barel123133@gmail.com>
2025-03-09 17:16:33 +0200

    chore: add env for server url `SERVER_URL`

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Your backend server address
+        target: process.env.SERVER_URL || 'http://localhost:3000', // Your backend server address
         changeOrigin: true,
         secure: false,
       },
