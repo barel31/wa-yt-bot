4510f36a14629073ecdc1afd975eb3c60f8fd44b
barel31 <barel123133@gmail.com>
2025-03-09 17:43:04 +0200

    fix: update env extraction

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,22 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_SERVER_URL || 'http://localhost:3000', // Your backend server address
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on current mode
+  const env = loadEnv(mode, process.cwd(), '');
+  console.log('VITE_SERVER_URL:', env.VITE_SERVER_URL); // Debug log
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URL || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+  };
+});
